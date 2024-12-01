046a464c2c6632b8a666cf493c592439b3e94a45
Sina <sinaderik7@gmail.com>
2024-12-01 10:30:56 +0330

    app file is edited

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Angular from './components/Angular'
 export default function App() {
   return (
     <div>
+      <h3>This is vueJs</h3>
       <VueJs />
       <hr />
+      <h3>This is angular</h3>
       <Angular />
     </div>
   )
